e7a2139c8fc8210ed481961c26f44fac0cc99370
Coder160 <coder160@2806-10a6-0020-1d42-11eb-4bb2-b66a-d934.ipv6.infinitum.net.mx>
2023-05-27 23:37:25 -0600

    start_project

---
+++ README.md
@@ -0,0 +1,21 @@
+#FAST_API_PYTHON_TEMPLATE
+
+
+#pre-requisites:
+-python3
+-pip
+
+##Clone repository:
+
+git clone https://github.com/coder160/fastapi_python_template
+
+##Enter directory:
+
+cd fast_api_sa
+
+## create virtual enviroment and install dependencies
+pip install pipenv 
+pipenv install -r requirements.txt
+
+##run app:
+pipenv run python app.py
---
+++ app.py
@@ -0,0 +1,20 @@
+from typing import Union
+
+import uvicorn as SERVER
+from fastapi import FastAPI as APP
+
+_app = APP()
+_server = SERVER
+
+
+@_app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@_app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    _server.run(_app, host="localhost", port=9028)
